2a5bb673a3bbdb70b6f7da69c47b99ce54cdbd8f
Bernasss12 <tudo.pt.minecraft@gmail.com>
2019-05-07 17:28:02 +0100

    Tinkers's Construct #11
    Items - Food some
    Items - Shortbow finished

--- logger.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET /P _filename= Please enter folder:
-SET _path=%~dp0
-@ECHO OFF
-robocopy "%_path:~0,-1%\TODO\%_filename%\textures" "%_path:~0,-1%\assets\%_filename%\textures" /L /TS /XO /MIR /LOG:%_path:~0,-1%\logs\%_filename%_textures_log.txt
-if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel%
-)
-PAUSE >nul
